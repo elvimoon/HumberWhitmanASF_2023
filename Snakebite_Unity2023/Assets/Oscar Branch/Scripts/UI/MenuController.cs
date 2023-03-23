1fed33ce94552e201581bdad3366fd82f634e3d5
JreedVio <jreedvio1376@gmail.com>
2023-03-22 23:56:34 -0400

    Finished Option Menu and Added Credits Scene

--- Snakebite_Unity2023/Assets/Oscar Branch/Scripts/UI/MenuController.cs
+++ Snakebite_Unity2023/Assets/Oscar Branch/Scripts/UI/MenuController.cs
@@ -6,13 +6,18 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     public string gameScene;
+    public string creditsScene;
     public void NewGame()
     {
         SceneManager.LoadScene(gameScene, LoadSceneMode.Single);
     }
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene(creditsScene, LoadSceneMode.Single);
+    }
 
     public void Quit()
     {
-            Application.Quit();
+        Application.Quit();
     }
 }
